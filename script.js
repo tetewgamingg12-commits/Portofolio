c80d2ebc215551fe1533324365db9f79727f6229
tetewgamingg12-commits <tetewgamingg12@gmail.com>
2025-10-30 11:12:09 +0700

    Fix event listener syntax for greeting button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //1. Pilih tombol untuk ID-nya
 const tombolSapa = document.getElementById('sapaButton')
 //2. Tambahkan 'event listener' untuk aksi 'click'
-tombolSapa.addEventListener('click,function'(){
-  //3. Jalankan tombol ini ketika di klik alert('Terimakasih Sudah berkunjung!');
+tombolSapa.addEventListener('click',function(){
+  //3. Jalankan kode ini ketika tombol di klik alert('Terimakasih Sudah berkunjung!');
 });
