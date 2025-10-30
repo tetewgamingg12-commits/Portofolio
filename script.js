812e0c7593c442905f9f9871f42398377370e471
tetewgamingg12-commits <tetewgamingg12@gmail.com>
2025-10-30 11:07:38 +0700

    Add click event listener to greeting button

---
+++ script.js
@@ -0,0 +1,6 @@
+//1. Pilih tombol untuk ID-nya
+const tombolSapa = document.getElementById('sapaButton')
+//2. Tambahkan 'event listener' untuk aksi 'click'
+tombolSapa.addEventListener('click,function'(){
+  //3. Jalankan tombol ini ketika di klik alert('Terimakasih Sudah berkunjung!');
+});
